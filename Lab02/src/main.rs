e27a5961a9ded038affe8b04e49d4899c1cbcb95
CiocanRazvan <ciocanrazvan4@gmail.com>
2025-10-09 15:43:30 +0300

    Laboratorul 2

--- Lab02/src/main.rs
+++ Lab02/src/main.rs
@@ -43,68 +43,58 @@ fn main() {
 
 //Problema 3
 
-fn add_str(s: &mut String, string: &str){
+fn add_str(s: &mut String, string: &str) {
     s.push_str(&string);
 }
 
-fn add_space(s: &mut String, n: u32)
-{
-    for _ in 1..=n
-    {
+fn add_space(s: &mut String, n: u32) {
+    for _ in 1..=n {
         s.push(' ');
     }
 }
 
-fn add_integer(s: &mut String, int: u32)
-{
+fn add_integer(s: &mut String, int: u32) {
     let mut n = int;
     let mut i = 0;
-    while n != 0
-    {
+    while n != 0 {
         n = n / 10;
-        i=i+1;
+        i = i + 1;
     }
-    i = i%3;
-    if i == 0
-    {
+    i = i % 3;
+    if i == 0 {
         i = 3;
     }
     n = int;
     let mut ogl = 0;
-    while n != 0
-    {
-        ogl = ogl*10 + n%10;
-        n = n/10;
+    while n != 0 {
+        ogl = ogl * 10 + n % 10;
+        n = n / 10;
     }
     let mut j = 0;
     let mut i1 = i;
-    while ogl != 0
-    {
-        if j == i1
-        {
+    while ogl != 0 {
+        if j == i1 {
             s.push('_');
-            i1=-1;
-            j=0;
-        }
-        else if j == 3
-        {
+            i1 = -1;
+            j = 0;
+        } else if j == 3 {
             s.push('_');
-            j=0;
+            j = 0;
         }
-        let a = ogl%10;
+        let a = ogl % 10;
         let c = (a as u8 + b'0') as char;
         s.push(c);
-        ogl = ogl /10;
-        j = j+1;
+        ogl = ogl / 10;
+        j = j + 1;
     }
 }
-fn main(){
+fn main() {
     let mut s = String::from("");
     add_str(&mut s, "Most");
     add_space(&mut s, 12);
     add_str(&mut s, "crate");
     add_space(&mut s, 7);
-    add_integer(&mut s,34238908);
+    add_integer(&mut s, 34238908);
     add_space(&mut s, 12);
     add_str(&mut s, "and");
     add_space(&mut s, 7);
@@ -121,6 +111,6 @@ fn main(){
     add_str(&mut s, "the");
     add_space(&mut s, 12);
     add_str(&mut s, "version");
-    
+
     println!("{s}");
 }

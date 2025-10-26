f75257f9c0de50da06f8f05b6f36f425256d4911
CiocanRazvan <ciocanrazvan4@gmail.com>
2025-10-26 17:01:15 +0200

    Delete Lab04/target directory

--- Lab04/target/debug/deps/Lab04-b59c719983303cb3.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Razvan\LabRust\Lab04\target\debug\deps\Lab04-b59c719983303cb3.d: src\main.rs
-
-c:\Users\Razvan\LabRust\Lab04\target\debug\deps\Lab04-b59c719983303cb3.exe: src\main.rs
-
-src\main.rs:
--- Lab04/target/debug/deps/Lab04.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Razvan\LabRust\Lab04\target\debug\deps\Lab04.d: src\main.rs
-
-C:\Users\Razvan\LabRust\Lab04\target\debug\deps\Lab04.exe: src\main.rs
-
-src\main.rs:
